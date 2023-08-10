35aaf23ac134384e4f2ac454911e970104a22fb3
Maxim Kim <exynil@gmail.com>
2023-08-10 15:03:36 +0600

    refactor

--- app/services/redis_storage_manager.py
+++ app/services/redis_storage_manager.py
@@ -6,14 +6,14 @@ class CodeSnapManager:
         self.redis_storage = redis_storage
         self.prefix = "codesnaps"
 
-    def get_key(self, training_session_id, alias):
+    def get_key(self, training_session_id, alias) -> str:
         return f"{self.prefix}:{training_session_id}:{alias}"
 
-    def set(self, training_session_id, alias, code):
+    def set(self, training_session_id, alias, code) -> None:
         key = self.get_key(training_session_id, alias)
-        return self.redis_storage.set_value(key, code)
+        self.redis_storage.set_value(key, code)
 
-    def get(self, training_session_id, alias):
+    def get(self, training_session_id, alias) -> str:
         key = self.get_key(training_session_id, alias)
         code = self.redis_storage.get_value(key)
         return code if code else ""
@@ -24,14 +24,14 @@ class ControllerManager:
         self.redis_storage = redis_storage
         self.prefix = "controllers"
 
-    def get_key(self, training_session_id):
+    def get_key(self, training_session_id) -> str:
         return f"{self.prefix}:{training_session_id}"
 
-    def set(self, training_session_id, owner):
+    def set(self, training_session_id, owner) -> None:
         key = self.get_key(training_session_id)
-        return self.redis_storage.set_value(key, owner)
+        self.redis_storage.set_value(key, owner)
 
-    def get(self, training_session_id):
+    def get(self, training_session_id) -> str:
         key = self.get_key(training_session_id)
         owner = self.redis_storage.get_value(key)
         return owner if owner else ""
