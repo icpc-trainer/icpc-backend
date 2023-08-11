3d1e5d1a1e0546a2cb274b1a837142023544639f
slonoboy <aslanbek230@gmail.com>
2023-08-11 19:12:39 +0600

    remove problem table and get comments api

--- app/db/models/submission.py
+++ app/db/models/submission.py
@@ -1,17 +1,15 @@
-from sqlalchemy import TEXT, UUID, ForeignKey
+from sqlalchemy import TEXT, UUID, ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import BaseTable
 from .training_session import TrainingSession
-from .problem import Problem
 
 
 class Submission(BaseTable):
     __tablename__ = "submissions"
 
-    problem_id: Mapped[UUID] = mapped_column(
-        UUID(as_uuid=True),
-        ForeignKey("problems.id"),
+    problem_alias: Mapped[str] = mapped_column(
+        String(255),
     )
     training_session_id: Mapped[UUID] = mapped_column(
         UUID(as_uuid=True),
@@ -21,5 +19,4 @@ class Submission(BaseTable):
         TEXT,
     )
 
-    problem: Mapped[Problem] = relationship(backref="submissions", lazy="joined")
     training_session: Mapped[TrainingSession] = relationship(backref="submissions", lazy="joined")
