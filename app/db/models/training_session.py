09cd0936ba1cc30ad34bdcbfae720c40aedffaeb
Peskov Sergey <peskovdev@proton.me>
2023-08-13 13:08:09 +0600

    Update UUID model mapping

--- app/db/models/training_session.py
+++ app/db/models/training_session.py
@@ -1,3 +1,5 @@
+from uuid import UUID as _UUID
+
 from sqlalchemy import UUID, Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -10,11 +12,11 @@ from app.db.enums import TrainingStatusEnum
 class TrainingSession(BaseTable):
     __tablename__ = "training_sessions"
 
-    contest_id: Mapped[UUID] = mapped_column(
+    contest_id: Mapped[_UUID] = mapped_column(
         UUID(as_uuid=True),
         ForeignKey("contests.id"),
     )
-    team_id: Mapped[UUID] = mapped_column(
+    team_id: Mapped[_UUID] = mapped_column(
         UUID(as_uuid=True),
         ForeignKey("teams.id"),
     )
