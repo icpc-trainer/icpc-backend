dc08a9906c8cc81ba890d4ca264bb426409a9f2d
Maxim Kim <exynil@gmail.com>
2023-08-18 15:31:15 +0600

    get contest standings endpoint

--- app/endpoints/contest/standings.py
+++ app/endpoints/contest/standings.py
@@ -20,3 +20,15 @@ async def get_my_standings(
 ) -> dict:
     result = await proxy_manager.get_my_standing(contest_id)
     return result
+
+
+@router.get(
+    "/{contest_id}/standings",
+    status_code=status.HTTP_200_OK,
+)
+async def get_contest_standings(
+    contest_id: int,
+    proxy_manager: ProxyManager = Depends(ProxyManager),
+) -> dict:
+    result = await proxy_manager.get_contest_standings(contest_id)
+    return result
