675521fa5fdf474bce830c885b5a2ffbe4a47b79
Peskov Sergey <peskovdev@proton.me>
2023-08-20 19:00:27 +0600

    Add contest_fininshed

--- app/background_tasks/finish_training.py
+++ app/background_tasks/finish_training.py
@@ -12,13 +12,13 @@ async def training_session_finisher(
     wait_time: timedelta,
     training_session_id: UUID,
     training_session_repository: TrainingSessionRepository,
-):
+) -> None:
     await asyncio.sleep(wait_time.total_seconds())
     await training_session_repository.complete_training_session(
         training_session_id=training_session_id
     )
     message = WebSocketMessage(
-        type=MessageTypeEnum.TRAINING_FINISHED,
-        payload={},
+        type=MessageTypeEnum.CONTEST_FINISHED,
+        payload=None,
     )
     await training_manager.broadcast(str(training_session_id), message.json())
